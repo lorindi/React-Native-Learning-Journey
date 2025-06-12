eb831e007c636d0b16264a72f943f1605475f49e
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-06-12 16:35:19 +0300

    Exploring Layouts & Flexbox

--- section-2-basics-goals-app/goals-app/App.js
+++ section-2-basics-goals-app/goals-app/App.js
@@ -1,22 +1,23 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{margin: 16, borderWidth: 2, borderColor:'blue', padding: 16 }}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Button title="Tap me!" />
-
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder="Your course goal" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of goals ...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  appContainer: {
+    backgroundColor: '#FAFAFA',
+    padding:50
+  }
 });
